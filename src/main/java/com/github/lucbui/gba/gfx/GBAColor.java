021ea11a58b4a895fb18929490cd406c29d66a1b
Justis Lamanna <lamanna.justis@gmail.com>
2018-10-19 22:59:38 -0500

    Added equals to GBAColor

--- src/main/java/com/github/lucbui/gba/gfx/GBAColor.java
+++ src/main/java/com/github/lucbui/gba/gfx/GBAColor.java
@@ -120,6 +120,21 @@ public class GBAColor {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GBAColor gbaColor = (GBAColor) o;
+        return red == gbaColor.red &&
+                green == gbaColor.green &&
+                blue == gbaColor.blue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(red, green, blue);
+    }
+
+    @Override
     public String toString() {
         return "GBAColor{" +
                 "red=" + red +
