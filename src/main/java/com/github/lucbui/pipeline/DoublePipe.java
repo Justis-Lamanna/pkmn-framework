94895733822bd38701b94cde09bb0dd1018f23ac
Justis Lamanna <lamanna.justis@gmail.com>
2019-01-22 19:57:09 -0600

    Initial shuffle to keep core and gba modules separate

--- src/main/java/com/github/lucbui/pipeline/DoublePipe.java
+++
@@ -1,8 +0,0 @@
-package com.github.lucbui.pipeline;
-
-/**
- * A pipe which functions both as a reader and a writer
- * @param <T>
- */
-public interface DoublePipe<T> extends ReadPipe<T>, WritePipe<T> {
-}
