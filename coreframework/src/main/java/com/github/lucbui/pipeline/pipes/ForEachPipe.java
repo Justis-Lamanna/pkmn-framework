f72a7bed9f1e4ed3466c712f044a86dd023acd11
Justis Lamanna <lamanna.justis@gmail.com>
2019-01-24 21:20:28 -0600

    Documentation + safety for ForEachPipe

--- coreframework/src/main/java/com/github/lucbui/pipeline/pipes/ForEachPipe.java
+++ coreframework/src/main/java/com/github/lucbui/pipeline/pipes/ForEachPipe.java
@@ -8,24 +8,50 @@ import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+/**
+ * A pipe which iterates over an object's properties, and placing each through a sub pipeline
+ * @param <O> The input object type
+ * @param <S> The output object type
+ */
 public class ForEachPipe<O, S> implements DoublePipe<O> {
 
     private Function<? super O, Stream<? extends S>> converter;
     private Pipeline<? super S> subPipeline;
 
+    /**
+     * Define a ForEachPipe
+     * @param converter The function which takes the incoming object, and converts it into a stream of sub-objects
+     * @param subPipeline The pipeline to run each sub-object through.
+     */
     public ForEachPipe(Function<? super O, Stream<? extends S>> converter, Pipeline<S> subPipeline){
         this.converter = Objects.requireNonNull(converter);
         this.subPipeline = Objects.requireNonNull(subPipeline);
     }
 
+    /**
+     * Extract a stream of subobjects from an object, and run each through the sub-pipeline.
+     * During iteration, each run through the sub-pipeline gets its own copy of the iterator passed into this
+     * pipeline. This prevents "cross-contamination" between subpipes, which would cause hard-to-find bugs.
+     * @param object The object to modify
+     * @param iterator The iterator to read from
+     * @param pkmnFramework The PkmnFramework running this code
+     */
     @Override
     public void read(O object, HexFieldIterator iterator, PkmnFramework pkmnFramework) {
-        converter.apply(object).forEach(i -> subPipeline.modify(iterator, i, pkmnFramework));
+        converter.apply(object).forEach(i -> subPipeline.modify(iterator.copy(), i, pkmnFramework));
     }
 
+    /**
+     * Write a stream of sub-objects from an object, and run each through the sub-pipeline.
+     * During iteration, each run through the sub-pipeline gets its own copy of the iterator passed into this
+     * pipeline. This prevents "cross-contamination" between subpipes, which would cause hard-to-find bugs.
+     * @param iterator The iterator to read from
+     * @param object The object to modify
+     * @param pkmnFramework The PkmnFramework running this code
+     */
     @Override
     public void write(HexFieldIterator iterator, O object, PkmnFramework pkmnFramework) {
-        converter.apply(object).forEach(i -> subPipeline.write(iterator, i, pkmnFramework));
+        converter.apply(object).forEach(i -> subPipeline.write(iterator.copy(), i, pkmnFramework));
     }
 
     @Override
